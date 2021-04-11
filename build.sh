6b57a1ddc4c47273435cf617c5a1d69e790ffa52
Zireael07 <zi@mail.com>
2021-04-11 17:47:15 +0200

    new: initial wasm-bindgen setup

---
+++ build.sh
@@ -0,0 +1,8 @@
+# build
+#cargo build --target wasm32-unknown-unknown --release
+
+# build wasm to www
+# file name matches output file (cargo package name) above
+#wasm-bindgen ./target/wasm32-unknown-unknown/release/rust_web_roguelike.wasm --out-dir docs --web --no-typescript
+
+wasm-pack build --out-name rust_web_text_based --out-dir docs --target web --no-typescript
---
+++ docs/README.md
@@ -0,0 +1,3 @@
+rust-web-text-based
+
+Taking the idea of rust-web-roguelike-HTML, which evolved into something closer to a roguelike-MUD hybrid, but this time closer to the other end of the spectrum.
