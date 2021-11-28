f7d614b8f046eec1eef8300647a066be52405a14
Zireael07 <katarzynamosz@gmail.com>
2021-11-28 11:53:26 +0000

    Deploying to gh-pages from  @ 0d5fc46db34baa1375323e3c409001256c0205c9 🚀

--- rust_web_text_based.js
+++ rust_web_text_based.js
@@ -597,7 +597,7 @@ async function init(input) {
         var ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper938 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper937 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 172, __wbg_adapter_22);
         return addHeapObject(ret);
     };
